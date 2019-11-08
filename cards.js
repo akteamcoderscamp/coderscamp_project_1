500f44137406a6624bb0e72a6a8c27e2a7ba0d7f
dzasta997 <justynaturowska97@gmail.com>
2019-11-08 21:17:23 +0100

    added winner function

--- cards.js
+++ cards.js
@@ -1,13 +1,7 @@
-
-
-
-
 let kier=[two={value:2}, three={value:3}, four={value:4}, five={value:5}, six={value:6}, seven={value:7}, eight={value:8}, nine={value:9},ten={value:10}, jack={value:10}, queen={value:10}, king={value:10}, ace={ value:11}];
 let pik=[two={value:2}, three={value:3}, four={value:4}, five={value:5}, six={value:6}, seven={value:7}, eight={value:8}, nine={value:9},ten={value:10}, jack={value:10}, queen={value:10}, king={value:10}, ace={ value:11}];
 let trefl=[two={value:2}, three={value:3}, four={value:4}, five={value:5}, six={value:6}, seven={value:7}, eight={value:8}, nine={value:9},ten={value:10}, jack={value:10}, queen={value:10}, king={value:10}, ace={ value:11}];
 let karo=[two={value:2}, three={value:3}, four={value:4}, five={value:5}, six={value:6}, seven={value:7}, eight={value:8}, nine={value:9},ten={value:10}, jack={value:10}, queen={value:10}, king={value:10}, ace={ value:11}];
-
-
 
 function game(){
     //losuje kolor
@@ -28,7 +22,6 @@ function game(){
     function cardDraw(color){
         return  num = Math.floor(Math.random()*color.length);
     }
-
     let cardColor = colorDraw();
     let number=cardDraw(cardColor);
     var figure = cardColor.slice(number,number+1)
@@ -36,4 +29,12 @@ function game(){
     return figure;
 }
 
+function winner( playerScore, computerScore){
+    if (computerScore>21){return 'win'}
+    if (playerScore>21){return 'lose'}
+    if (playerScore>computerScore){
+        return 'win'} else if (computerScore===playerScore){
+            return 'draw'} else {
+                return 'lose'}
+}
 
