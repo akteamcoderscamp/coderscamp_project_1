e7aafe309552d66750f20c8f86fef4bcec955767
Izabela <iza7.12@tlen.pl>
2019-11-13 13:26:38 +0100

    Zmiana lekka w logice
    Chyba najlepiej jak na razie

--- blackjack_logic.js
+++ blackjack_logic.js
@@ -10,66 +10,93 @@ class playerClass{
 let dealer = new playerClass('Dealer');
 let player = new playerClass('Player');
 
+const checkBlackJack = (person) => person.cards.length == 2 && person.points == 21 ? true: false; 
 
 //definiowanie kart
 class card{
-    constructor(suit, name, value){
+    constructor(suit, name, value, cardImg){
     this.suit = suit;
     this.name = name;
     this.value = value;
+    this.cardImg = cardImg;
     }
 }
 
 var deck= new Array();
 var suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
 
-for (i=0; i<suits.length; i++){
-    for (j=2; j<15 ; j++){
-        if(j<11){
-            var value=j;
-            var name=value.toString();
-        }else if (j<14){
-            value=10;
-            switch (j){
-                case 11: name="J";
-                break;
-                case 12: name="Q";
-                break;
-                case 13: name="K";
-            }
-        }else {
-            value=[1, 11];
-            name="A"
-        }
-    const newCard = new card (suits[i], name, value);
-    deck.push(newCard)
-    }
-    
-}
-
 //okienko z wynikiem?
 let scoreWin = document.getElementById('win')
 scoreWin.hidden = true;
 let scoreLoose = document.getElementById('loose')
 scoreLoose.hidden = true;
-//gra
 
-function hold(){
-    if (player.points < 21 && dealer.points < 21 && dealer.points < player.points) {
-        cardDraw(dealer);
-        checkScore();
-} else {console.log('both hold')}
+//gra
+createDeck();
+settingCards();
+
+
+function createDeck(){
+    for (i=0; i<suits.length; i++){
+        for (j=2; j<15 ; j++){
+            if(j<11){
+                var value=j;
+                var name=value.toString();
+            }else if (j<14){
+                value=10;
+                switch (j){
+                    case 11: name="J";
+                    break;
+                    case 12: name="Q";
+                    break;
+                    case 13: name="K";
+                }
+            }else {
+                value=[1, 11];
+                name="A"
+            }
+        var cardImg = name + suits[i].charAt(0) + ".png";
+        const newCard = new card (suits[i], name, value, cardImg);
+
+        deck.push(newCard)
+        }
+        
+    }
+
 }
 
-function add(){
-    if (player.points < 21 && dealer.points < 21) {
+//we draw 2 cards for player and 1 for dealer(one that is hiden isn't draw yet - easier for showing score)
+function settingCards(){
         cardDraw(player);
+        cardDraw(player);
+        player.blackJack = checkBlackJack(player);
         cardDraw(dealer);
-        checkScore();
+    if (checkBlackJack(player)){
+        hold();
+    } 
+}
+
+
+function hold(){
+        if (dealer.cards.length = 1) {cardDraw(dealer)}  //draw 2nd dealer card - the dealers face-down card is turned up.
+        dealer.blackJack = checkBlackJack(dealer)
+        if (dealer.blackJack) checkScoreDealer();
+        while (dealer.points < 17){
+            cardDraw(dealer);
+            checkScoreDealer();
+        }
+    }
+
+        
+function add(){
+    if (player.points < 21) {
+        cardDraw(player);
+        checkScorePlayer();
 }
 }
 
 function cardDraw(person){
+    if (player.points <21 && dealer.points <21 && dealer.win == false && player.win == false){
     var num = Math.floor(Math.random()*deck.length);
     var drewCard = deck[num];
     if (drewCard.name === "A" && person.points > 10){
@@ -78,59 +105,50 @@ function cardDraw(person){
         drewCard.value = 11;}
     person.cards.push(drewCard);
     person.points = person.points + drewCard.value;
-    if (person.cards.length === 2 && person.points === 21) {person.blackJack = true};
     document.getElementById(person.name).innerHTML = person.points;
+    document.getElementById(person.name + "Card" + person.cards.length).src = "deck/" + drewCard.cardImg;
+    }
 }
 
-// function checkScore(){
-//     if ((player.points >= 17 && dealer.points >=17) || (player.points >=21 || dealer.points >=21)) { 
-//         if (player.blackJack == true && dealer.blackJack == false) {scoreWindow.innerHTML = "Player\'s BlackJack"}
-//         else if (dealer.blackJack == true && player.blackJack == false) {scoreWindow.innerHTML = "Dealer\'s BlackJack"}
-//         else if (player.blackJack == true && dealer.blackJack == true)
-//         {scoreWindow.innerHTML = "A tie"}
-//         else if (player.points > 21 || dealer.points > 21) {
-//             if (dealer.points > 21 && player.points > 21)
-//             {scoreWindow.innerHTML = "Nobody wins tonight..."}
-//             else if (dealer.points > 21 && player.points < 21)
-//             {scoreWindow.innerHTML = "Player winns!"}
-//             else if (dealer.points < 21 && player.points > 21)
-//             {scoreWindow.innerHTML = "Dealer won..."}
-//         }
-//         else {
-//             if (dealer.points == player.points)
-//             {scoreWindow.innerHTML = "A tie"}
-//             else if (dealer.points < player.points)
-//             {scoreWindow.innerHTML = "Player winns!"}
-//             else if (dealer.points > player.points)
-//             {scoreWindow.innerHTML = "Dealer won..."}   
-//             }
-//         scoreWindow.hidden = false;
-//     }    
-//     }
-function checkScore(){
-    if ((player.points >= 17 && dealer.points >=17) || (player.points >=21 || dealer.points >=21)) {
-        if (dealer.blackJack == true && player.blackJack == true) {player.win = true; dealer.win = true;
-        console.log('1')}
-        else if (dealer.blackJack == true) {dealer.win = true; console.log('2')}
-        else if (player.blackJack == true) {player.win = true; console.log('3')}
-        else if (player.points > 21 && dealer.points <= 21) {dealer.win = true; console.log('4')}
-        else if (player.points <= 21 && dealer.points > 21) {player.win = true; console.log('5')}
-        else if (player.points <=21 && dealer.points <=21) {
-            if (player.points > dealer.points) {player.win = trueconsole.log('6')}
-            else if (player.points < dealer.points) {dealer.win = true; console.log('7')}
-        } else {console.log('both over 21')}
 
-        if (player.win == true && dealer.win == true){
-        scoreLoose.innerHTML = "A tie";
+function checkScorePlayer(){
+    if (player.points > 21) {
+        dealer.win = true;
+        scoreLoose.innerHTML = "BUST - You lost";
         scoreLoose.hidden = false;
-        } else if (player.win == false && dealer.win == false) {
-        scoreLoose.innerHTML = "Nobody wins tonight";
+    } 
+}
+
+function checkScoreDealer(){
+    if (dealer.points > 21) {
+        player.win = true;
+        scoreWin.innerHTML = "You won!";
+        scoreWin.hidden = false;
+    } else if (player.blackJack == true && dealer.blackJack == false) {
+        player.win = true;
+        scoreWin.innerHTML = "You won!";
+         scoreWin.hidden = false;
+    } else if (player.blackJack == false && dealer.blackJack == true){
+        dealer.win = true;
+        scoreLoose.innerHTML = "The dealer won...";
         scoreLoose.hidden = false;
-        } else if (player.win == false && dealer.win == true)
-        {scoreLoose.innerHTML = "Dealer won...";
+    } else if (player.blackJack == true && dealer.blackjack == true){
+        dealer.win = true;
+        player.win = true;
+        scoreLoose.innerHTML = "A tie.";
+        scoreLoose.hidden = false;
+    } else if (dealer.points >= 17){
+        if (player.points > dealer.points) {
+       player.win = true;
+       scoreWin.innerHTML = "You won!";
+       scoreWin.hidden = false;
+        } else if (player.points < dealer.points){
+        dealer.win = true;
+        scoreLoose.innerHTML = "The dealer won...";
+        scoreLoose.hidden = false;
+        } else {dealer.win = true;
+        player.win = true;
+        scoreLoose.innerHTML = "A tie.";
         scoreLoose.hidden = false;}
-        else if (player.win == true && dealer.win == false){
-        scoreWin.innerHTML = "Player winns!";
-        scoreWin.hidden = false;}
-    }
-    }  
+    } 
+}
