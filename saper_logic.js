2870eebd030725405857d9585c9987198ea7963d
dzasta997 <justynaturowska97@gmail.com>
2019-11-14 04:38:07 +0100

    deleted unnecessary variables

--- saper_logic.js
+++ saper_logic.js
@@ -4,29 +4,26 @@ var gameBoard = new Array();
 var ifSquare = new Array();
 var numOfBombs=13;
 class square{
-    constructor(ifBomb, value){
+    constructor(ifBomb, value, checked){
     this.ifBomb=ifBomb;
     this.value=value;
+    this.checked=checked;
 }}
 //generowanie obiektów w tablicy//
 for (i=0; i<81; i++){
-    gameBoard[i]=new square(0,0)
+    gameBoard[i]=new square(0,0, false)
 }
 //losowanie bomb//
 while(numOfBombs>0){
     var randomSquare=Math.floor(Math.random()*81); 
-    if(ifSquare.includes(randomSquare)===false){ // sprawdza czy na polu jest bomba
-        let arrBoard= gameBoard[randomSquare]; //wyciąga obiekt z tablicy
-        arrBoard.ifBomb=1;
+    if(ifSquare.includes(randomSquare)===false){ // sprawdza czy na polu jest juz bomba
+        gameBoard[randomSquare].ifBomb=1; 
         ifSquare.push(randomSquare);
         numOfBombs --;
     }
 }
-//sprawdzenie sąsiadów
+//sprawdza czy na polu jest bomba
 function checkBomb(squareNum){
-    let arrBoard= gameBoard[squareNum]; //wyciąga obiekt z tablicy 
-if (arrBoard.ifBomb===1) return true;
+if (gameBoard[squareNum].ifBomb===1) return true;
 else return false;
 }
-
-console.log(checkBomb(7),ifSquare)
