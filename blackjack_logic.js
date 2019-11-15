5e73b5a6a2844dc863b11c738fb0eb73d4efaa8e
B <basiasiczek@gmail.com>
2019-11-15 13:26:20 +0100

    Fix Ace Points counting and add dynamic card creation
    adding changePointsForAces function that evaluates if Ace should be counted as 1 point
    Adding cards to the flexbox container is now creating dynamically

--- blackjack_logic.js
+++ blackjack_logic.js
@@ -54,7 +54,7 @@ function createDeck(){
                     case 13: name="K";
                 }
             }else {
-                value=[1, 11];
+                value=11; //Default for Ace will be 11, decreased later in a game to 1 if needed
                 name="A"
             }
         var cardImg = name + suits[i].charAt(0) + ".png";
@@ -72,7 +72,17 @@ function settingCards(){
         cardDraw(player);
         cardDraw(player);
         player.blackJack = checkBlackJack(player);
+        changePointsForAces(player);  //the case where player gets 2 Aces in firs round
         cardDraw(dealer);
+        //adding green card to the flexbox container
+        const newDiv = document.createElement('div');
+        const newImg = document.createElement('img');
+        newImg.src = "deck/green_back.png";
+        newImg.id = "hiddenCard";
+        newDiv.className = "card";
+        newDiv.appendChild(newImg);
+        document.getElementById("DealerCards").appendChild(newDiv);
+        
     if (checkBlackJack(player)){
         hold();
     } 
@@ -80,19 +90,24 @@ function settingCards(){
 
 
 function hold(){
-        if (dealer.cards.length === 1) {cardDraw(dealer)}  //draw 2nd dealer card - the dealers face-down card is turned up.
+        //remove hidden(green) card
+        document.getElementById("hiddenCard").remove();
+        //draw 2nd dealer card - the dealers face-down card is turned up.
+        if (dealer.cards.length === 1) {cardDraw(dealer)}  
         dealer.blackJack = checkBlackJack(dealer)
-        if (dealer.blackJack) checkScoreDealer();
         checkScoreDealer();
         while (dealer.points < 17){
-            cardDraw(dealer);
+            //TODO: make wait here around 0,5s so dealer cards will be noticed I tried setTimeout(cardDraw(dealer),500);     
+            cardDraw(dealer);      
             checkScoreDealer();
+
         }
     }
 
-        
+
+
 function add(){
-    if (player.points < 21) {
+   if (player.points < 21) {
         cardDraw(player);
         checkScorePlayer();
 }
@@ -102,19 +117,37 @@ function cardDraw(person){
     if (dealer.win == false && player.win == false){
     var num = Math.floor(Math.random()*deck.length);
     var drewCard = deck[num];
-    if (drewCard.name === "A" && person.points > 10){
-        drewCard.value = 1;
-    } else if (drewCard.name === "A" && person.points <= 10) {
-        drewCard.value = 11;}
+    deck.splice(num,1);  //remove the card from the deck
     person.cards.push(drewCard);
     person.points = person.points + drewCard.value;
     document.getElementById(person.name).innerHTML = person.points;
-    document.getElementById(person.name + "Card" + person.cards.length).src = "deck/" + drewCard.cardImg;
+
+    const newDiv = document.createElement('div');
+    const newImg = document.createElement('img');
+    newImg.src = "deck/" + drewCard.cardImg;
+    newDiv.className = "card"
+    newDiv.appendChild(newImg);
+    document.getElementById(person.name + "Cards").appendChild(newDiv);
+
+
+
 }
 }
 
+
+function changePointsForAces(person){
+    if(person.points > 21){
+        let ace = person.cards.find(ace => ace.value == 11); //find the Ace
+        if (typeof ace !== 'undefined'){  //if Ace was find
+            ace.value = 1;      //change it's value so it won't be found next time
+            person.points -=10;
+            document.getElementById(person.name).innerHTML = person.points;
+        }
+    }
+}
 
 function checkScorePlayer(){
+    changePointsForAces(player);
     if (player.points > 21) {
         dealer.win = true;
         scoreLoose.innerHTML = "BUST - You lost";
@@ -123,6 +156,7 @@ function checkScorePlayer(){
 }
 
 function checkScoreDealer(){
+    changePointsForAces(dealer);
     if (dealer.points > 21) {
         player.win = true;
         scoreWin.innerHTML = "You win! This time...";
@@ -159,3 +193,12 @@ function checkScoreDealer(){
         looseWindow.style.display = 'initial';}
     } 
 }
+
+function sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
+  }
