8bfd14701c5f583cbbb917915dcf71e9c1b92900
framista <adajab1@gmail.com>
2019-11-15 19:42:42 +0100

    improved working and design

--- saper_logic.js
+++ saper_logic.js
@@ -6,9 +6,8 @@ var numOfBombs = 13;
 const rows = 9;
 const columns = 9;
 const amountOfElements = rows * columns;
-var leftUpperSquare = [];
 var squareDivs = document.querySelectorAll("div");
-
+var amountOfShowedElements = 0;
 
 
 class Square {
@@ -22,7 +21,6 @@ class Square {
 /////////////////////////////////////////////////////////////////////////////////////
 /* tu funkcje definiowane */
 /////////////////////////////////////////////////////////////////////////////////////
-
 
 //sprawdza czy na polu jest bomba
 function checkBomb(squareNum) {
@@ -44,6 +42,7 @@ function countNeighbor() {
                 (isInBoard(x + 1, y + 1) | 0);
         }
     }
+
 }
 
 function isInBoard(x, y) {
@@ -52,65 +51,55 @@ function isInBoard(x, y) {
     }
 }
 
-function isInBoardBoolean(x, y) {
-    return (x >= 0 && y >= 0 && x < columns && y < rows) ? true : false;
-}
-
-//sprawdzamy gora dol prawo lewo
-// jezeli w ktoryms bomba to zwraca true
-function checkNeighbor(x, y, direction) {
-    // rowCurrent*columns+columnCurrent  <- określenie indeksu dla wiersza i kolumny
-    let indexUp = (x - 1) * columns + y;
-    let indexDown = (x + 1) * columns + y;
-    let indexRight = x * columns + y + 1;
-    let indexLeft = x * columns + y - 1;
-    switch (direction) {
-        case "up":
-            return (ifSquare.includes(indexUp) === true) ? true : false;
-        case "right":
-            return (ifSquare.includes(indexRight) === true) ? true : false;
-        case "down":
-            return (ifSquare.includes(indexDown) === true) ? true : false;
-        case "left":
-            return (ifSquare.includes(indexLeft) === true) ? true : false;
-
-    }
-}
-
-// x y - kliknięty element na poczatku
-function checkBorder(x, y) {
+function uncover(x, y) {
 
     gameBoard[x][y].checked = true;
-    if (leftUpperSquare[0] > x && leftUpperSquare[1] > y) {
-        leftUpperSquare[0] = x;
-        leftUpperSquare[1] = y;
+    removeFlag(x * columns + y);
+    squareDivs[x * columns + y].classList.add("showed");
+    amountOfShowedElements++;
+    if (gameBoard[x][y].value != 0) {
+        squareDivs[x * columns + y].innerHTML = gameBoard[x][y].value;
     }
-    if (isInBoardBoolean(x - 1, y) === true) {
-        if (checkNeighbor(x, y, "up") === false && gameBoard[x - 1][y].checked === false && gameBoard[x - 1][y].value == 0) {
-            checkBorder(x - 1, y);
-        }
-    }
-    if (isInBoardBoolean(x, y + 1) === true) {
-        if (checkNeighbor(x, y, "right") === false && gameBoard[x][y + 1].checked === false && gameBoard[x][y + 1].value == 0) {
-            checkBorder(x, y + 1);
-        }
-    }
-    if (isInBoardBoolean(x + 1, y) === true) {
-        if (checkNeighbor(x, y, "down") === false && gameBoard[x + 1][y].checked === false && gameBoard[x + 1][y].value == 0) {
-            checkBorder(x + 1, y);
-        }
-    }
-    if (isInBoardBoolean(x, y - 1) === true) {
-        if (checkNeighbor(x, y, "left") === false && gameBoard[x][y - 1].checked === false && gameBoard[x][y - 1].value == 0) {
-            checkBorder(x, y - 1);
-        }
+    win();
+    if (gameBoard[x][y].value == 0) {
+        if (y > 0 && gameBoard[x][y].checked === false)
+            uncover(x, y - 1);
+        if (y < (columns - 1) && gameBoard[x][y + 1].checked === false)
+            uncover(x, y + 1);
+        if (x < (rows - 1) && gameBoard[x + 1][y].checked === false)
+            uncover(x + 1, y);
+        if (x > 0 && gameBoard[x - 1][y].checked === false)
+            uncover(x - 1, y);
+        if (y > 0 && x > 0 && gameBoard[x - 1][y - 1].checked === false)
+            uncover(x - 1, y - 1);
+        if (y > 0 && x < (rows - 1) && gameBoard[x + 1][y - 1].checked === false)
+            uncover(x + 1, y - 1);
+        if (y < (columns - 1) && x < (rows - 1) && gameBoard[x + 1][y + 1].checked === false)
+            uncover(x + 1, y + 1);
+        if (y < (columns - 1) && x > 0 && gameBoard[x - 1][y + 1].checked === false)
+            uncover(x - 1, y + 1);
     }
 }
+
+function removeFlag(i) {
+    if (squareDivs[i].classList.contains("flag")) {
+        squareDivs[i].classList.remove("flag");
+    }
+}
+
+function win() {
+    let greenSquareTab = Array.from(squareDivs).filter(element => {
+        return element.classList.contains("showed");
+    });
+    if (greenSquareTab.length === amountOfElements - ifSquare.length) {
+        alert("WYGRANA")
+    }
+}
+
 
 /////////////////////////////////////////////////////////////////////////////////////
 /* tu już wywołanie kolejno funkcji */
 /////////////////////////////////////////////////////////////////////////////////////
-
 
 //tworzenie tablicy dwuwymiarowej
 for (let x = 0; x < rows; x++) {
@@ -161,19 +150,6 @@ for (let x = 0; x < rows; x++) {
     console.log(tempRow)
 }
 
-// 
-console.log("*******************************")
-// console.log(checkNeighbor(5,2))
-
-//
-
-leftUpperSquare.push(6, 6)
-// console.log(checkBorder(6, 6))
-// console.log("legtUppersquare" + leftUpperSquare[0] + "\t" + leftUpperSquare[1])
-
-odkryj(6, 6)
-//
-
 //wypisuje ładnie macierz z checked ;)
 console.log("wypisuje ładnie macierz z checked  ;)")
 for (let x = 0; x < rows; x++) {
@@ -184,35 +160,6 @@ for (let x = 0; x < rows; x++) {
     console.log(tempRow)
 }
 
-function odkryj(x, y) {
-
-    gameBoard[x][y].checked = true;
-    squareDivs[x * columns + y].classList.add("showed");
-    if (gameBoard[x][y].value != 0)
-        squareDivs[x * columns + y].innerHTML = gameBoard[x][y].value;
-
-    if (gameBoard[x][y].value == 0) {
-        if (y > 0 && gameBoard[x][y].checked === false)
-            odkryj(x, y - 1);
-        if (y < (columns - 1) && gameBoard[x][y + 1].checked === false)
-            odkryj(x, y + 1);
-        if (x < (rows - 1) && gameBoard[x + 1][y].checked === false)
-            odkryj(x + 1, y);
-        if (x > 0 && gameBoard[x - 1][y].checked === false)
-            odkryj(x - 1, y);
-        if (y > 0 && x > 0 && gameBoard[x - 1][y - 1].checked === false)
-            odkryj(x - 1, y - 1);
-        if (y > 0 && x < (rows - 1) && gameBoard[x + 1][y - 1].checked === false)
-            odkryj(x + 1, y - 1);
-        if (y < (columns - 1) && x < (rows - 1) && gameBoard[x + 1][y + 1].checked === false)
-            odkryj(x + 1, y + 1);
-        if (y < (columns - 1) && x > 0 && gameBoard[x - 1][y + 1].checked === false)
-            odkryj(x - 1, y + 1);
-
-    }
-
-}
-
 /////////////////////////////////////////
 //showing on website
 ///////////////////////////////////////
@@ -221,25 +168,28 @@ console.log(squareDivs)
 
 for (let i = 0; i < squareDivs.length; i++) {
     squareDivs[i].addEventListener("click", function () {
-        console.log(i);
+        console.log(amountOfShowedElements)
         // rowCurrent*columns+columnCurrent  <- określenie indeksu dla wiersza i kolumny
         let columnCurrent = i % columns;
         let rowCurrent = (i - columnCurrent) / rows;
-        console.log("bomba" + gameBoard[rowCurrent][columnCurrent].ifBomb);
         if (gameBoard[rowCurrent][columnCurrent].ifBomb === 1) {
-            squareDivs[i].classList.add("bomb");
+            for (let j = 0; j < ifSquare.length; j++) {
+                removeFlag(ifSquare[j]);
+                squareDivs[ifSquare[j]].classList.add("bomb");
+            }
             alert('KONIEC');
-
         } else if (gameBoard[rowCurrent][columnCurrent].value != 0) {
+            removeFlag(i);
             squareDivs[i].classList.add("showed");
             squareDivs[i].innerHTML = gameBoard[rowCurrent][columnCurrent].value;
+            amountOfShowedElements++;
+            win();
         } else {
-            odkryj(rowCurrent, columnCurrent);
+            uncover(rowCurrent, columnCurrent);
         }
     });
     squareDivs[i].addEventListener("contextmenu", function () {
         squareDivs[i].classList.add("flag");
-        // return false; //dislable default right click menu
     });
 }
 
