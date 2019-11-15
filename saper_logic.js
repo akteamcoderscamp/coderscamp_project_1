50bfa54aff6f2cbe30083d82011ffe181824256a
framista <adajab1@gmail.com>
2019-11-15 15:40:41 +0100

    First partially version of showing empty area

--- saper_logic.js
+++ saper_logic.js
@@ -16,7 +16,7 @@ class Square {
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
-/* tu funkcje definiowane */ 
+/* tu funkcje definiowane */
 /////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -30,26 +30,77 @@ function countNeighbor() {
     for (let x = 0; x < rows; x++) {
         for (let y = 0; y < columns; y++) {
             gameBoard[x][y].value =
-                (checkNeighbor(x - 1, y - 1) | 0) +
-                (checkNeighbor(x - 1, y) | 0) +
-                (checkNeighbor(x - 1, y + 1) | 0) +
-                (checkNeighbor(x, y - 1) | 0) +
-                (checkNeighbor(x, y + 1) | 0) +
-                (checkNeighbor(x + 1, y - 1) | 0) +
-                (checkNeighbor(x + 1, y) | 0) +
-                (checkNeighbor(x + 1, y + 1) | 0);
+                (isInBoard(x - 1, y - 1) | 0) +
+                (isInBoard(x - 1, y) | 0) +
+                (isInBoard(x - 1, y + 1) | 0) +
+                (isInBoard(x, y - 1) | 0) +
+                (isInBoard(x, y + 1) | 0) +
+                (isInBoard(x + 1, y - 1) | 0) +
+                (isInBoard(x + 1, y) | 0) +
+                (isInBoard(x + 1, y + 1) | 0);
         }
     }
 }
 
-function checkNeighbor(x, y) {
+function isInBoard(x, y) {
     if (x >= 0 && y >= 0 && x < columns && y < rows) {
         return gameBoard[x][y].ifBomb;
     }
 }
 
+function isInBoardBoolean(x, y) {
+    return (x >= 0 && y >= 0 && x < columns && y < rows) ? true : false;
+}
+
+//sprawdzamy gora dol prawo lewo
+// jezeli w ktoryms bomba to zwraca true
+function checkNeighbor(x, y, direction) {
+    // rowCurrent*columns+columnCurrent  <- określenie indeksu dla wiersza i kolumny
+    let indexUp = (x - 1) * columns + y;
+    let indexDown = (x + 1) * columns + y;
+    let indexRight = x * columns + y + 1;
+    let indexLeft = x * columns + y - 1;
+    switch (direction) {
+        case "up":
+            return (ifSquare.includes(indexUp) === true) ? true : false;
+        case "right":
+            return (ifSquare.includes(indexRight) === true) ? true : false;
+        case "down":
+            return (ifSquare.includes(indexDown) === true) ? true : false;
+        case "left":
+            return (ifSquare.includes(indexLeft) === true) ? true : false;
+
+    }
+}
+
+// x y - kliknięty element na poczatku
+function checkBorder(x, y) {
+
+    gameBoard[x][y].checked = true;
+    if (isInBoardBoolean(x - 1, y) === true) {
+        if (checkNeighbor(x, y, "up") === false && gameBoard[x - 1][y].checked === false && gameBoard[x - 1][y].value == 0) {
+            checkBorder(x - 1, y);
+        } else if (checkNeighbor(x, y, "up") === false && gameBoard[x - 1][y].checked === false) { gameBoard[x - 1][y].checked = true; }
+    }
+    if (isInBoardBoolean(x, y + 1) === true) {
+        if (checkNeighbor(x, y, "right") === false && gameBoard[x][y + 1].checked === false && gameBoard[x][y + 1].value == 0) {
+            checkBorder(x, y + 1);
+        } else if (checkNeighbor(x, y, "right") === false && gameBoard[x][y + 1].checked === false) { gameBoard[x][y + 1].checked = true; }
+    }
+    if (isInBoardBoolean(x + 1, y) === true) {
+        if (checkNeighbor(x, y, "down") === false && gameBoard[x + 1][y].checked === false && gameBoard[x + 1][y].value == 0) {
+            checkBorder(x + 1, y);
+        } else if (checkNeighbor(x, y, "down") === false && gameBoard[x + 1][y].checked === false) { gameBoard[x + 1][y].checked = true; }
+    }
+    if (isInBoardBoolean(x, y - 1) === true) {
+        if (checkNeighbor(x, y, "left") === false && gameBoard[x][y - 1].checked === false && gameBoard[x][y - 1].value == 0) {
+            checkBorder(x, y - 1);
+        } else if (checkNeighbor(x, y, "up") === false && gameBoard[x][y - 1].checked === false) { gameBoard[x][y - 1].checked = true; }
+    }
+}
+
 /////////////////////////////////////////////////////////////////////////////////////
-/* tu już wywołanie kolejno funkcji */ 
+/* tu już wywołanie kolejno funkcji */
 /////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -78,7 +129,7 @@ while (numOfBombs > 0) {
 countNeighbor();
 
 /////////////////////////////////////////////////////////////////////////////////////
-/* tu sobie testuje, czemu nie działa */ 
+/* tu sobie testuje, czemu nie działa */
 /////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -98,6 +149,21 @@ for (let x = 0; x < rows; x++) {
     let tempRow = "";
     for (let y = 0; y < columns; y++) {
         tempRow += gameBoard[x][y].value + "\t"
+    }
+    console.log(tempRow)
+}
+
+// 
+console.log("*******************************")
+// console.log(checkNeighbor(5,2))
+console.log(checkBorder(3, 2))
+
+//wypisuje ładnie macierz z checked ;)
+console.log("wypisuje ładnie macierz z checked  ;)")
+for (let x = 0; x < rows; x++) {
+    let tempRow = "";
+    for (let y = 0; y < columns; y++) {
+        tempRow += gameBoard[x][y].checked + "\t"
     }
     console.log(tempRow)
 }
